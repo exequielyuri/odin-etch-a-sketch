532cd53ec7b6eabd3a04627aa2cae3d90578d9c8
Exequiel Garcia <leiuqxre@gmail.com>
2022-04-12 17:00:50 +0800

    Add settings for changing number of pixels

--- script.js
+++ script.js
@@ -3,25 +3,36 @@ function changeBoardSize() {
     grid.style.setProperty("width", size);
 }
 
+function deleteCells() {
+    cells.forEach(cell => grid.removeChild(cell));
+    cells = [];
+}
+
+function createGrid(dimension=16) { // default: 16*16
+    for (let i=0; i < dimension**2; i++) {
+        cells.push(document.createElement("div"));
+    }
+
+    cells.forEach((cell) => {
+        cell.classList.add("cell");
+        cell.addEventListener('mouseover', ()=>cell.style.cssText = `opacity: 1`);
+        grid.appendChild(cell);
+    });
+
+    grid.style.cssText = `grid-template-columns: repeat(${dimension}, auto)`;
+}
 
 const grid = document.getElementById("grid");
 const board = document.getElementById("board");
 const sizeSlider = document.getElementById("size-slider");
+const pixelSlider = document.getElementById("pixel-slider");
+const colorPicker = document.getElementById("color-picker");
 
-const dimension = 5;
-const cells = [];
-const penColor = "green";
-
-for (let i=0; i < dimension**2; i++) {
-    cells.push(document.createElement("div"));
-}
-
-cells.forEach((cell) => {
-    cell.classList.add("cell");
-    cell.addEventListener('mouseover', ()=>cell.style.cssText = `background-color: ${penColor}`);
-    grid.appendChild(cell);
-});
-
-grid.style.cssText = `grid-template-columns: repeat(${dimension}, auto)`;
+let cells = [];
+createGrid();
 
 sizeSlider.addEventListener('input', changeBoardSize);
+pixelSlider.addEventListener('mouseup', () => {
+    deleteCells();
+    createGrid(pixelSlider.value);
+});
